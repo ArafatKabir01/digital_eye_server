985e2496329431916a6f625432d8d11eedb0dd36
ak nabil <nabil786.ak@gmail.com>
2022-09-07 15:11:52 +0600

    mongodb claster connection done

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
 // Middleware 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = "mongodb+srv://digitaleye07:8nZd3yeoQuYbQSsX@cluster1.helve.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+ console.log("ok")
+});
+
 
 app.get('/' , (req , res) => {
     res.send('server is running');
